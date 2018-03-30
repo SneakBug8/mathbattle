0680b01a165d0ae998084164d4420b74ac9541f4
SneakBug8 <sneakbug8@gmail.com>
2018-03-30 20:41:03 +0300

    upd

--- src/config/GlobalConfig.cs
+++ src/config/GlobalConfig.cs
@@ -1,5 +1,6 @@
 namespace mathbattle.config {
     public static class GlobalConfig {
         public static string Token = "386013421:AAHo6qF6ccQ8Lxpb5uf2PO5KOE-brSz2voU";
+        public static int LoopDelay = 1000;
     }
 }
